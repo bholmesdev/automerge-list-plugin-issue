aa40d55fe3bd07f83bc83177baecf561992c45e9
bholmesdev <bholmesdev@gmail.com>
2024-08-13 08:58:15 -0400

    feat: title editing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,54 @@
-import { consoleLogSink, Replicache, type ReplicacheOptions } from "replicache";
+import {
+  consoleLogSink,
+  Replicache,
+  type ReplicacheOptions,
+  type WriteTransaction,
+} from "replicache";
 import { REPLICACHE_LICENSE_KEY } from "./consts";
 import Slugger from "github-slugger";
-import { createSignal } from "solid-js";
+import { createResource, createSignal, Show } from "solid-js";
+import type { Node } from "@markdoc/markdoc";
+
+type Block = string;
+
+type Entry = {
+  title: string;
+  tags: string[];
+  body: Block[];
+};
 
 const rep = new Replicache({
   licenseKey: REPLICACHE_LICENSE_KEY,
   // TODO: users
   name: "global-user",
   mutators: {
-    async setEntry(tx, entry: { title: string; body: string }) {
+    async setEntry(
+      tx: WriteTransaction,
+      entry: { title: string; body: string },
+    ) {
       const id = new Slugger().slug(entry.title);
       await tx.set(id, entry);
+    },
+    async createDraftEntry(tx: WriteTransaction) {
+      const existingDraft = await tx.get("entry:draft");
+      if (existingDraft) return existingDraft;
+
+      await tx.set("entry:draft", {
+        title: "Untitled",
+        tags: [],
+        body: ["Start typing..."],
+      });
+
+      return tx.get("entry:draft");
+    },
+    async updateEntryTitle(tx: WriteTransaction, title: string) {
+      const draft = await tx.get("entry:draft");
+      if (!draft) throw new Error("No draft found.");
+
+      await tx.set("entry:draft", {
+        ...draft,
+        title,
+      });
     },
   },
 });
@@ -22,27 +60,30 @@ const repSignal = (id: string) => {
   return value;
 };
 
-let count = 0;
-
 export function App() {
-  const value = repSignal("test");
+  const [draft] = createResource(rep.mutate.createDraftEntry);
 
   return (
     <main class="max-w-prose">
       <p>{rep.idbName}</p>
-      <h1 contentEditable>{value()?.title}</h1>
-      <div contentEditable>{value()?.body}</div>
-
-      <button
-        onClick={() =>
-          rep.mutate.setEntry({
-            title: "Test",
-            body: `The count is ${count++}`,
-          })
-        }
-      >
-        Update entry
-      </button>
+      <Show when={draft()}>
+        {(value) => (
+          <>
+            <h1
+              contentEditable
+              onInput={(e) =>
+                rep.mutate.updateEntryTitle(e.target.textContent ?? "")
+              }
+              class="text-2xl font-bold"
+            >
+              {value().title}
+            </h1>
+            {value().body.map((text) => (
+              <p contentEditable>{text}</p>
+            ))}
+          </>
+        )}
+      </Show>
     </main>
   );
 }
