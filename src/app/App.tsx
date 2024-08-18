6c91eae569937e6419e5cc30486d395baba03394
bholmesdev <bholmesdev@gmail.com>
2024-08-18 18:01:11 -0400

    fix: focus behavior when adding a link

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
 import { createResource, createSignal, Show, Suspense } from "solid-js";
 import { createID, rep } from "./cache.js";
-import { EditorState, type Command } from "prosemirror-state";
+import { EditorState, TextSelection, type Command } from "prosemirror-state";
 import { EditorView } from "prosemirror-view";
 import { undo, redo, history } from "prosemirror-history";
 import { keymap } from "prosemirror-keymap";
 import { baseKeymap } from "prosemirror-commands";
-import { type MarkType, type Attrs } from "prosemirror-model";
+import { type MarkType, type Attrs, Mark } from "prosemirror-model";
 import {
   headingShortcutPlugin,
   listShortcutPlugin,
@@ -13,7 +13,7 @@ import {
 } from "./plugins.js";
 import { schema } from "./schema.js";
 import { LinkPopover } from "./link/popover.jsx";
-import { linkView } from "./link/view.jsx";
+import { linkView, setActiveMark } from "./link/view.jsx";
 
 export function App() {
   return (
@@ -56,12 +56,17 @@ const Suspended = () => {
       keymap({
         "Mod-z": undo,
         "Mod-y": redo,
-        "Mod-b": toggleMark(schema.marks.strong),
-        "Mod-i": toggleMark(schema.marks.em),
-        "Mod-shift-h": toggleMark(schema.marks.highlight),
-        "Mod-shift-k": toggleMark(schema.marks.link, {
-          href: "https://example.com",
-        }),
+        "Mod-b": toggleMark(schema.marks.strong.create()),
+        "Mod-i": toggleMark(schema.marks.em.create()),
+        "Mod-shift-h": toggleMark(schema.marks.highlight.create()),
+        "Mod-shift-k": (state, dispatch) => {
+          const mark = schema.marks.link.create({
+            href: "",
+          });
+          const handled = toggleMark(mark, "focus-end")(state, dispatch);
+          if (handled) setActiveMark(mark);
+          return handled;
+        },
       }),
       keymap(baseKeymap),
     ],
@@ -108,7 +113,10 @@ const Suspended = () => {
 
 // Learned about $from.marks() from source:
 // https://github.com/ProseMirror/prosemirror-commands/blob/master/src/commands.ts#L602
-function toggleMark(mark: MarkType, attrs?: Attrs): Command {
+function toggleMark(
+  mark: Mark,
+  selectionBehavior: "preserve" | "focus-end" = "preserve",
+): Command {
   return (state, dispatch): boolean => {
     if (!dispatch) return false;
 
@@ -116,7 +124,7 @@ function toggleMark(mark: MarkType, attrs?: Attrs): Command {
     if (mark.isInSet(state.storedMarks ?? $from.marks())) {
       dispatch(state.tr.removeStoredMark(mark));
     } else {
-      dispatch(state.tr.addStoredMark(mark.create(attrs)));
+      dispatch(state.tr.addStoredMark(mark));
     }
 
     if (state.selection.from === state.selection.to) return true;
@@ -131,19 +139,13 @@ function toggleMark(mark: MarkType, attrs?: Attrs): Command {
       }
       return !isAlreadyToggled;
     });
-    if (isActive) {
-      dispatch(
-        state.tr.removeMark(state.selection.from, state.selection.to, mark),
-      );
-    } else {
-      dispatch(
-        state.tr.addMark(
-          state.selection.from,
-          state.selection.to,
-          mark.create(attrs),
-        ),
-      );
+    let tr = isActive
+      ? state.tr.removeMark(state.selection.from, state.selection.to, mark)
+      : state.tr.addMark(state.selection.from, state.selection.to, mark);
+    if (selectionBehavior === "focus-end") {
+      tr = tr.setSelection(TextSelection.create(tr.doc, state.selection.to));
     }
+    dispatch(tr);
     return true;
   };
 }
